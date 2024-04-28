a6e4166c22fb4f3d718cd707b68d3715ab14257e
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2024-04-28 11:56:44 +0700

    Rampung

---
+++ src/main/java/com/p06/P06.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.p06;
+
+/**
+ *
+ * @author Hanif
+ */
+public class P06 {
+
+    public static void main(String[] args) {
+        CRUDMongoDB aplikasi = new CRUDMongoDB();
+        aplikasi.setVisible(true);
+    }
+}
